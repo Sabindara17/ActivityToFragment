6bcf8ed4f29fc4f3b288ca286d0765f54cb75b8e
aashutosh <aashutosh.poudel8@gmail.com>
2023-05-04 07:04:31 +0545

    gridview example

--- app/src/main/java/com/example/activitytofragment/MainActivity.java
+++ app/src/main/java/com/example/activitytofragment/MainActivity.java
@@ -13,32 +13,49 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private MyAdapter adapter;
-    private final ArrayList<DataClass> data = new ArrayList<DataClass>();
-        String tutorial[]={
-                "1",
-                "2",
-                "3",
-                "4"
-        };
+//    private final ArrayList<DataClass> data = new ArrayList<DataClass>();
+ ArrayList<DataClass>  shyameList = new ArrayList<DataClass>();
+    String tutorial[] = {
+            "1",
+            "2",
+            "3",
+            "4"
+    };
+
+
     ArrayAdapter<String> arrayAdapter;
-        ListView listView;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView= findViewById(R.id.list);
-        data.add(new DataClass(R.drawable.ic_launcher_background, "Item 1"));
-        data.add(new DataClass(R.drawable.ic_launcher_background, "Item 2"));
-        data.add(new DataClass(R.drawable.ic_launcher_background, "Item 3"));
-        adapter= new MyAdapter(MainActivity.this,data);
-        listView.setAdapter(adapter);
+        listView = findViewById(R.id.list);
+        shyameList.add(new DataClass(R.drawable.ic_launcher_background,"asdasdsadsa"));
+        shyameList.add(new DataClass(R.drawable.ic_launcher_background,"kljklhgkjlhgk"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "asdasdasdas 1"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "Item 2"));
+//        data.add(new DataClass(R.drawable.ic_launcher_background, "Item 3"));
 
-//        arrayAdapter= new ArrayAdapter<>(
+
+       MyAdapter adapter= new MyAdapter(MainActivity.this,shyameList);
+//        listView.setAdapter(adapter);
+
+//      ArrayAdapter<String>  arrayAdapter= new ArrayAdapter<>(
 //                this,
-//                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+//               androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
 //                tutorial
 //        );
-//        listView.setAdapter(arrayAdapter);
+
+        ArrayAdapter<String> rameAdapter = new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, tutorial);
+        listView.setAdapter(rameAdapter);
 //
 //        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
--- app/src/main/java/com/example/activitytofragment/MyAdapter.java
+++ app/src/main/java/com/example/activitytofragment/MyAdapter.java
@@ -27,23 +27,37 @@ public class MyAdapter extends ArrayAdapter<DataClass> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View newViewclass = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+       DataClass  getDataPosition = getItem(position);
 
-        // convertView which is recyclable view
-        View currentItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+       ImageView imageView = newViewclass.findViewById(R.id.imageView);
+        TextView textView = newViewclass.findViewById(R.id.titleTextView);
 
-        // get the position of the view from the ArrayAdapter
-        DataClass currentNumberPosition = getItem(position);
-
-        // then according to the position of the view assign the desired image for the same
-        ImageView numbersImage = currentItemView.findViewById(R.id.imageView);
-        numbersImage.setImageResource(currentNumberPosition.images);
-
-        // then according to the position of the view assign the desired TextView 1 for the same
-        TextView textView1 = currentItemView.findViewById(R.id.titleTextView);
-        textView1.setText(currentNumberPosition.getText());
-
-
-        // then return the recyclable view
-        return currentItemView;
+        imageView.setImageResource(getDataPosition.images);
+        textView.setText(getDataPosition.text);
+        return super.getView(position, convertView, parent);
     }
+
+    //    @NonNull
+//    @Override
+//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//
+//        // convertView which is recyclable view
+//        View currentItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+//
+//        // get the position of the view from the ArrayAdapter
+//        DataClass currentNumberPosition = getItem(position);
+//
+//        // then according to the position of the view assign the desired image for the same
+//        ImageView numbersImage = currentItemView.findViewById(R.id.imageView);
+//        numbersImage.setImageResource(currentNumberPosition.images);
+//
+//        // then according to the position of the view assign the desired TextView 1 for the same
+//        TextView textView1 = currentItemView.findViewById(R.id.titleTextView);
+//        textView1.setText(currentNumberPosition.getText());
+//
+//
+//        // then return the recyclable view
+//        return currentItemView;
+//    }
 }
